ab04934499994cd42bb134a46c0d7ced796a1c98
Eric Cecchi <eric.cecchi@gmail.com>
2015-05-28 22:09:29 -0500

    Add link to Home from internal page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,29 @@ var {Link} = Router,
     Icon = require('./Icon');
 
 var Navbar = React.createClass({
+  mixins: [ Router.State ],
 
   render: function() {
     var cx = React.addons.classSet;
     var navbarClasses = cx({
-          'mainnav': true
-        });
+      'mainnav': true
+    });
+    var navbarStyles = {
+      homeLink: {
+        display: this.isActive('/') ? 'none' : 'block'
+      }
+    }
 
     return (
       <header className={navbarClasses}>
         <div className="row">
           <div className="col _span-12">
+            <nav className="mainnav-nav _left">
+              <ul className="mainnav-links" style={navbarStyles.homeLink}>
+                <li><Link to="/" className="button _inverse _pill _previous">GoldenRipe Home</Link></li>
+              </ul>
+            </nav>
+
             <nav className="mainnav-nav _right">
               <ul className="mainnav-links">
                 <li><a href="//theway.goldenripe.org" className="button _inverse _pill _next">Go to The Way</a></li>
